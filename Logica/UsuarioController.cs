a860de36281043a49d8bfb58ef69a5d239cda49d
Marl <winering@gmail.com>
2024-10-22 18:18:37 -0300

    Commit 27

--- Logica/UsuarioController.cs
+++ Logica/UsuarioController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,21 @@ namespace proyecto_final_club_deportivo.Logica
         
         public DataTable login(string username, string password)
         {
-            return this.usuarioRepository.loginUsuario(username, password);
+            string pass = getSHA256(password);
+            return this.usuarioRepository.loginUsuario(username, pass);
+        }
+
+
+        // Encripta la contraseña con un algoritmo SHA256
+        public static string getSHA256(string str)
+        {
+            SHA256 sha256 = SHA256.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
         }
     }
 }
