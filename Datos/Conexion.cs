5502c1c354ac0c6224b704821fb908358adcc74c
Marl <winering@gmail.com>
2024-11-17 12:09:48 -0300

    Commit 55

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 
 /**
@@ -52,6 +53,55 @@ namespace proyecto_final_club_deportivo.Datos
             this.usuario = "ugjrogu4xztm9uix";
             this.clave = "HJFkxQONPWlyE13xxf7u";
         }
+
+        /*private Conexion() // asignamos valores a las variables de la conexion
+        {
+            // variables usadas para larepetición de líneas de código
+            bool correcto = false;
+            int mensaje;
+            // creamos las variables para recibir los datos desde el teclado
+
+            string T_servidor = "Servidor" ;
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave";
+
+            while (correcto != true)
+            {
+                // Armamos los cuadros de dialogo para el ingreso de datos
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese servidor", "DATOS DE INSTALACIÓN MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese puerto", "DATOS DE INSTALACIÓN MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese usuario", "DATOS DE INSTALACIÓN MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox
+                ("ingrese clave", "DATOS DE INSTALACIÓN MySQL");
+
+                mensaje = (int)MessageBox.Show("su ingreso: SERVIDOR = " +
+                T_servidor + " PUERTO= " + T_puerto + " USUARIO: " +
+                T_usuario + " CLAVE: " + T_clave,
+                "AVISO DEL SISTEMA", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+                
+                if (mensaje != 6) // el valor 6 corresponde al SI
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
+
+            // reemplazamos los datos concretos que teniamos por las variables
+            this.baseDatos = "proyecto_club_deportivo";
+            this.servidor = T_servidor; // "localhost";
+            this.puerto = T_puerto; //"3306";
+            this.usuario = T_usuario; // "root";
+            this.clave = T_clave; // "";
+        }*/
 
         // proceso de interacción
         public MySqlConnection CrearConexion()
